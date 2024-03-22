73af1ba2d483a2bbe4c90a8b5a83451bd92d4f9b
Abhilasha Purwar <abhilasha.purwar@gmail.com>
2024-03-23 02:44:53 +0530

    Connected Prisma to Postgres with Supabase

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ npm install
 
 (faced bugs in installing shadcn, solved with following code)
 npm install -g shadcn-ui@latest
+
+npm install pg
+npm install @prisma/adapter-pg
+
+npm install dotenv
+npm install @prisma/adapter-neon @neondatabase/serverless ws
+npm install --save-dev @types/ws
+npm update -g prisma
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,30 @@
-import { PrismaClient } from '@prisma/client'
-import "server-only"
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import "server-only";
+
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 declare global {
   // eslint-disable-next-line no-var, no-unused-vars
-  var cachedPrisma: PrismaClient
+  var cachedPrisma: PrismaClient;
 }
 
-let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
 } else {
   if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
+    global.cachedPrisma = new PrismaClient();
   }
-  prisma = global.cachedPrisma
+  prisma = global.cachedPrisma;
 }
 
-export const db = prisma
+export const db = prisma;
